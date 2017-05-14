71b9538d25dfcf6d70631d986fc3bb0e4cb3e118
Anja Berens <pharaun666@gmail.com>
2017-05-13 23:28:09 -0700

    view matrix

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,110 @@
 #[macro_use]
 extern crate glium;
-extern crate image;
+//extern crate image;
 
 
-#[derive(Copy, Clone)]
-struct Vertex {
-    position: [f32; 2],
-    tex_coords: [f32; 2],
-}
-
-implement_vertex!(Vertex, position, tex_coords);
+mod teapot;
 
 
 fn main() {
     use glium::{DisplayBuild, Surface};
-    let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
+    let display = glium::glutin::WindowBuilder::new().with_depth_buffer(24).build_glium().unwrap();
 
-    let vertex1 = Vertex { position: [-0.5, -0.5], tex_coords: [0.0, 0.0] };
-    let vertex2 = Vertex { position: [ 0.5, -0.5], tex_coords: [0.0, 1.0] };
-    let vertex3 = Vertex { position: [-0.5,  0.5], tex_coords: [1.0, 0.0] };
-    let vertex4 = Vertex { position: [ 0.5,  0.5], tex_coords: [1.0, 1.0] };
-    let shape = vec![vertex1, vertex2, vertex3, vertex4];
+    let positions = glium::VertexBuffer::new(&display, &teapot::VERTICES).unwrap();
+    let normals = glium::VertexBuffer::new(&display, &teapot::NORMALS).unwrap();
+    let indices = glium::IndexBuffer::new(&display, glium::index::PrimitiveType::TrianglesList, &teapot::INDICES).unwrap();
 
-    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
-    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TriangleStrip);
+    //use std::io::Cursor;
+    //let image = image::load(Cursor::new(&include_bytes!("dva.png")[..]), image::PNG).unwrap().to_rgba();
+    //let image_dimensions = image.dimensions();
+    //let image = glium::texture::RawImage2d::from_raw_rgba_reversed(image.into_raw(), image_dimensions);
+    //let texture = glium::texture::Texture2d::new(&display, image).unwrap();
 
-    use std::io::Cursor;
-    let image = image::load(Cursor::new(&include_bytes!("dva.png")[..]), image::PNG).unwrap().to_rgba();
-    let image_dimensions = image.dimensions();
-    let image = glium::texture::RawImage2d::from_raw_rgba_reversed(image.into_raw(), image_dimensions);
-
-    let texture = glium::texture::Texture2d::new(&display, image).unwrap();
-
-    let mut t: f32 = -0.5;
 
     let vertex_shader_src = r#"
         #version 140
 
-        in vec2 position;
+        in vec3 position;
 
-        in vec2 tex_coords;
-        out vec2 v_tex_coords;
+        in vec3 normal;
+        out vec3 v_normal;
 
+        uniform mat4 perspective;
         uniform mat4 matrix;
 
         void main() {
-            v_tex_coords = tex_coords;
-            gl_Position = matrix * vec4(position, 0.0, 1.0);
+            v_normal = transpose(inverse(mat3(matrix))) * normal;
+            gl_Position = perspective * matrix * vec4(position, 1.0);
         }
     "#;
 
     let fragment_shader_src = r#"
         #version 140
 
-        in vec2 v_tex_coords;
+        in vec3 v_normal;
         out vec4 color;
-
-        uniform sampler2D tex;
+        uniform vec3 u_light;
 
         void main() {
-            color = texture(tex, v_tex_coords);
+            float brightness = dot(normalize(v_normal), normalize(u_light));
+            vec3 dark_color = vec3(0.6, 0.0, 0.0);
+            vec3 regular_color = vec3(1.0, 0.0, 0.0);
+            color = vec4(mix(dark_color, regular_color, brightness), 1.0);
         }
     "#;
 
     let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
 
     loop {
-        // we update `t`
-        t += 0.0002;
-        if t > 0.5 {
-            t = -0.5;
-        }
+        let light = [-1.0, 0.4, 0.9f32];
 
-        let uniforms = uniform! {
-            matrix: [
-                [ t.cos(), t.sin(), 0.0, 0.0],
-                [-t.sin(), t.cos(), 0.0, 0.0],
-                [0.0, 0.0, 1.0, 0.0],
-                [ t , 0.0, 0.0, 1.0f32],
-            ],
-            tex: &texture,
+        let matrix = [
+            [0.01, 0.0, 0.0, 0.0],
+            [0.0, 0.01, 0.0, 0.0],
+            [0.0, 0.0, 0.01, 0.0],
+            [0.0, 0.0, 2.0, 1.0f32]
+        ];
+
+        let params = glium::DrawParameters {
+            depth: glium::Depth {
+                test: glium::draw_parameters::DepthTest::IfLess,
+                write: true,
+                .. Default::default()
+            },
+            .. Default::default()
         };
 
         let mut target = display.draw();
-        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        //target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
+
+
+        let perspective = {
+            let (width, height) = target.get_dimensions();
+            let aspect_ratio = height as f32 / width as f32;
+
+            let fov: f32 = 3.141592 / 3.0;
+            let zfar = 1024.0;
+            let znear = 0.1;
+
+            let f = 1.0 / (fov / 2.0).tan();
+
+            [
+                [f *   aspect_ratio   ,    0.0,              0.0              ,   0.0],
+                [         0.0         ,     f ,              0.0              ,   0.0],
+                [         0.0         ,    0.0,  (zfar+znear)/(zfar-znear)    ,   1.0],
+                [         0.0         ,    0.0, -(2.0*zfar*znear)/(zfar-znear),   0.0],
+            ]
+        };
+
         //target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
         //target.draw(&vertex_buffer, &indices, &program, &uniform! { t: t }, &Default::default()).unwrap();
-        target.draw(&vertex_buffer, &indices, &program, &uniforms, &Default::default()).unwrap();
+        //target.draw(&vertex_buffer, &indices, &program, &uniforms, &Default::default()).unwrap();
+        //target.draw((&positions, &normals), &indices, &program, &uniform! { matrix: matrix }, &Default::default()).unwrap();
+        //target.draw((&positions, &normals), &indices, &program, &uniform! { matrix: matrix, u_light: light }, &Default::default()).unwrap();
+        //target.draw((&positions, &normals), &indices, &program, &uniform! { matrix: matrix, u_light: light }, &params).unwrap();
+        target.draw((&positions, &normals), &indices, &program, &uniform! { matrix: matrix, perspective: perspective, u_light: light }, &params).unwrap();
+
         target.finish().unwrap();
 
         for ev in display.poll_events() {
