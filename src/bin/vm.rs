b02c4751c9c24fb947ab1543bf39cf603c9c8709
Anja Berens <pharaun666@gmail.com>
2017-12-26 01:56:33 -0700

    add an parser generator

--- src/bin/vm.rs
+++ src/bin/vm.rs
@@ -1,3 +1,14 @@
+use std::str;
+
+
 fn main() {
+    let test_asm: &'static str = include_str!("test.asm");
     println!("Hello");
+}
+
+
+
+#[test]
+fn comment_test() {
+	assert_eq!(";;", ";;");
 }
