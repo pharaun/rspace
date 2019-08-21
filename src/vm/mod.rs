e5a94e6b85072aa9f31659af230f8bcac1d95454
Anja Berens <pharaun666@gmail.com>
2019-08-21 00:28:58 -0700

    dev notes

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -5,6 +5,40 @@ pub mod opcode;
 pub mod cpu;
 
 
+// TODO: design ideas/how
+//
+// We need to have a way for various component to have these features:
+// - memory read
+// - memory write
+// - csr read
+// - csr write
+//
+// By maybe making mem read and write traits that should let us have a nicer
+// way to maybe do memory map ie
+//
+// 0x0000 -> 0x00FF = Rom (memory read trait)
+// 0x00FF -> 0x0F00 = Ram (Memory read/write trait)
+// 0x0F00 -> 0x1000 = Null (No read/write trait?)
+// 0x1000 -> 0x2000 = Memory Mapped I/O (Memory read/write trait)
+//
+// Then later for CSR that ie affect the component for eg the timer
+//
+// TimerInterrupt:
+// - CSR read
+// - CSR write
+// - Mem Read
+// - Mem Write
+//
+// For ie the timer interrupt that has memory i/o for configuration + reading data and
+// csr for triggering the timer interrupt (?)
+//
+// Cpu:
+// - CSR read
+// - CSR write
+//
+// For bits (such as counters) that would be readable/usable from the cpu for eg (simple cycle
+// counters)
+
 pub struct Emul32 {
     mem: mem::Memory,
     csr: csr::Csr,
