3408be52a172e8c311f1b0071d442bf892198a94
Anja Berens <pharaun666@gmail.com>
2019-08-29 22:44:58 -0700

    libfuzzer setup for emu32

---
+++ fuzz/fuzz_targets/fuzzer_emul32_rom.rs
@@ -0,0 +1,18 @@
+#![no_main]
+#[macro_use]
+extern crate libfuzzer_sys;
+extern crate rspace;
+
+fuzz_target!(|data: &[u8]| {
+    // fuzzed code goes here
+    let rom = {
+        let mut rom: [u8; 4096] = [0; 4096];
+        for i in 0..data.len() {
+            rom[i] = data[i];
+        }
+        rom
+    };
+
+    let mut vm = rspace::vm::Emul32::new_with_rom(rom);
+    vm.run();
+});
