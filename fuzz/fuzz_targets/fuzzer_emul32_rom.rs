f5893a468c82d82b1ab25f55308317fa9fefc2ed
Anja Berens <pharaun666@gmail.com>
2019-09-23 20:32:52 -0700

    Fuzzing target improvement

--- fuzz/fuzz_targets/fuzzer_emul32_rom.rs
+++ fuzz/fuzz_targets/fuzzer_emul32_rom.rs
@@ -15,8 +15,12 @@ fuzz_target!(|data: &[u8]| {
     // fuzzed code goes here
     let mut vm = rspace::vm::Emul32::new_with_rom(rom);
 
-    // Can't infinite run, so do a hundred (?) steps then call it quits
-    for _ in 0..200 {
-        vm.step();
+    // Can't infinite run, so do a thousand (?) steps then call it quits
+    // exit early if Trap fires
+    for _ in 0..1024 {
+        match vm.step() {
+            Ok(_)   => (),
+            Err(_)  => break,
+        }
     }
 });
