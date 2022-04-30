208609212e03e1770cc379242aad7551d6e04119
Anja Berens <pharaun666@gmail.com>
2022-04-29 21:57:07 -0700

    Simple rotating and drifting ships that wraps around the border

--- rcore/src/main.rs
+++ rcore/src/main.rs
@@ -1,11 +1,15 @@
 use bevy::prelude::*;
 use bevy_prototype_lyon::prelude::*;
+use std::iter::zip;
 
 #[derive(Component)]
 struct Ship;
 
 #[derive(Component)]
 struct Velocity(Vec2);
+
+#[derive(Component)]
+struct Rotation(f32);
 
 // TODO:
 // - faction
@@ -14,22 +18,27 @@ struct Velocity(Vec2);
 // - radar + shield -> Arc (direction + arc width)
 
 fn add_ships(mut commands: Commands) {
-    // TODO: draw an actual ship
-    let shape = shapes::RegularPolygon {
-        sides: 6,
-        feature: shapes::RegularPolygonFeature::Radius(20.0),
-        ..shapes::RegularPolygon::default()
+    let path = {
+        let mut path = PathBuilder::new();
+        let _ = path.move_to(Vec2::new(0.0, 20.0));
+        let _ = path.line_to(Vec2::new(10.0, -20.0));
+        let _ = path.line_to(Vec2::new(0.0, -10.0));
+        let _ = path.line_to(Vec2::new(-10.0, -20.0));
+        let _ = path.close();
+        path.build()
     };
 
     let poss = vec![Vec2::new(50.0, 200.0), Vec2::new(300.0, 0.0)];
+    let velo = vec![Vec2::new(-3.0, 1.0), Vec2::new(-2.0, -3.0)];
+    let roto = vec![1.0, 2.0];
 
-    for pos in poss {
+    for (pos, (vel, rot)) in zip(poss, zip(velo, roto)) {
         commands
             .spawn_bundle(GeometryBuilder::build_as(
-                &shape,
+                &path,
                 DrawMode::Outlined {
                     fill_mode: FillMode::color(Color::CYAN),
-                    outline_mode: StrokeMode::new(Color::BLACK, 5.0),
+                    outline_mode: StrokeMode::new(Color::BLACK, 2.0),
                 },
                 Transform {
                     translation: pos.extend(0.0),
@@ -37,23 +46,52 @@ fn add_ships(mut commands: Commands) {
                 },
             ))
             .insert(Ship)
-            .insert(Velocity(Vec2::new(1.0, 5.0)));
+            .insert(Velocity(vel))
+            .insert(Rotation(rot));
     }
 }
 
 struct VelocityTimer(Timer);
 fn apply_velocity(
+    windows: Res<Windows>,
     time: Res<Time>,
     mut timer: ResMut<VelocityTimer>,
     mut query: Query<(&Velocity, &mut Transform)>
 ) {
+    let window = windows.get_primary().unwrap();
     if timer.0.tick(time.delta()).just_finished() {
         for (vec, mut tran) in query.iter_mut() {
-            println!("POS - x: {}, y: {}", tran.translation.x, tran.translation.y);
-            println!("VEC - x: {}, y: {}", vec.0.x, vec.0.y);
-
             tran.translation.x += vec.0.x;
             tran.translation.y += vec.0.y;
+
+            // Wrap it if needed
+            if tran.translation.y < -(window.height() as f32 / 2.0) {
+                tran.translation.y += window.height() as f32;
+            } else if tran.translation.y > (window.height() as f32 / 2.0) {
+                tran.translation.y -= window.height() as f32;
+            }
+
+            if tran.translation.x < -(window.width() as f32 / 2.0) {
+                tran.translation.x += window.width() as f32;
+            } else if tran.translation.x > (window.width() as f32 / 2.0) {
+                tran.translation.x -= window.width() as f32;
+            }
+        }
+    }
+}
+
+struct RotationTimer(Timer);
+fn apply_rotation(
+    time: Res<Time>,
+    mut timer: ResMut<RotationTimer>,
+    mut query: Query<(&mut Rotation, &mut Transform)>
+) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for (mut rot, mut tran) in query.iter_mut() {
+            tran.rotation = Quat::from_rotation_z(
+                rot.0
+            );
+            rot.0 += 0.0174533;
         }
     }
 }
@@ -63,8 +101,10 @@ impl Plugin for ShipPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugin(ShapePlugin)
             .add_startup_system(add_ships)
-            .insert_resource(VelocityTimer(Timer::from_seconds(1.0, true)))
-            .add_system(apply_velocity);
+            .insert_resource(VelocityTimer(Timer::from_seconds(1.0 / 10.0, true)))
+            .insert_resource(RotationTimer(Timer::from_seconds(1.0 / 30.0, true)))
+            .add_system(apply_velocity)
+            .add_system(apply_rotation);
     }
 }
 
