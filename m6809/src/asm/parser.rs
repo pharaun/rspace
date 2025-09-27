b76f2100c7153dc15ba5683e882182edd160397d
Anja Berens <pharaun666@gmail.com>
2025-09-26 23:54:46 -0700

    Now work on the numbers

--- m6809/src/asm/parser.rs
+++ m6809/src/asm/parser.rs
@@ -10,6 +10,8 @@ use nom::{
       map_res,
       opt,
       map,
+      value,
+      success,
   },
   branch::alt,
   sequence::{
@@ -27,28 +29,28 @@ use crate::asm::ast;
 
 pub fn implict(input: &str) -> IResult<&str, ast::IInst> {
     alt((
-        map(tag("ABX"),  |_| ast::IInst::ABX),
-        map(tag("DAA"),  |_| ast::IInst::DAA),
-        map(tag("MUL"),  |_| ast::IInst::MUL),
-        map(tag("NOP"),  |_| ast::IInst::NOP),
-        map(tag("RTI"),  |_| ast::IInst::RTI),
-        map(tag("RTS"),  |_| ast::IInst::RTS),
-        map(tag("SEX"),  |_| ast::IInst::SEX),
-        map(tag("SEXW"), |_| ast::IInst::SEXW),
-        map(tag("SWI"),  |_| ast::IInst::SWI),
-        map(tag("SWI2"), |_| ast::IInst::SWI2),
-        map(tag("SWI3"), |_| ast::IInst::SWI3),
-        map(tag("SYNC"), |_| ast::IInst::SYNC),
+        value(ast::IInst::ABX,  tag("ABX")),
+        value(ast::IInst::DAA,  tag("DAA")),
+        value(ast::IInst::MUL,  tag("MUL")),
+        value(ast::IInst::NOP,  tag("NOP")),
+        value(ast::IInst::RTI,  tag("RTI")),
+        value(ast::IInst::RTS,  tag("RTS")),
+        value(ast::IInst::SEX,  tag("SEX")),
+        value(ast::IInst::SEXW, tag("SEXW")),
+        value(ast::IInst::SWI,  tag("SWI")),
+        value(ast::IInst::SWI2, tag("SWI2")),
+        value(ast::IInst::SWI3, tag("SWI3")),
+        value(ast::IInst::SYNC, tag("SYNC")),
     )).parse(input)
 }
 
 fn implict_imm(input: &str) -> IResult<&str, (ast::ImmInst, u8)> {
     let (input, inst) = alt((
-        map(tag("ANDCC"), |_| ast::ImmInst::ANDCC),
-        map(tag("BITMD"), |_| ast::ImmInst::BITMD),
-        map(tag("CWAI"),  |_| ast::ImmInst::CWAI),
-        map(tag("LDMD"),  |_| ast::ImmInst::LDMD),
-        map(tag("ORCC"),  |_| ast::ImmInst::ORCC),
+        value(ast::ImmInst::ANDCC, tag("ANDCC")),
+        value(ast::ImmInst::BITMD, tag("BITMD")),
+        value(ast::ImmInst::CWAI,  tag("CWAI")),
+        value(ast::ImmInst::LDMD,  tag("LDMD")),
+        value(ast::ImmInst::ORCC,  tag("ORCC")),
     )).parse(input)?;
 
     map(u8_imm, |imm| (inst, imm)).parse(input)
@@ -66,6 +68,27 @@ fn u32_imm(input: &str) -> IResult<&str, u8> {
     Ok((input, 0))
 }
 
+fn radix(input: &str) -> IResult<&str, u32> {
+    alt((
+        value(16, tag("0x")),
+        value(8,  tag("0o")),
+        value(2,  tag("0b")),
+        success(10),
+    )).parse(input)
+}
+
+fn number(input: &str) -> IResult<&str, u32> {
+    let (input, radix) = radix.parse(input)?;
+
+    let is_digit = |c: char| c.is_digit(radix);
+    let from_digit = |s: &str| u32::from_str_radix(s, radix);
+    map_res(take_while1(is_digit), from_digit).parse(input)
+}
+
+
+
+
+
 
 #[cfg(test)]
 mod test_parser {
@@ -79,5 +102,15 @@ mod test_parser {
     #[test]
     fn test_imm_implict() {
         assert_eq!(implict_imm("ANDCC 0xFF"), Ok(("", (ast::ImmInst::ANDCC, 0xFF))));
+    }
+
+    #[test]
+    fn test_radix() {
+        assert_eq!(radix("0xFF"), Ok(("FF", 16)));
+    }
+
+    #[test]
+    fn test_radix_fallback() {
+        assert_eq!(radix("123"), Ok(("123", 10)));
     }
 }
